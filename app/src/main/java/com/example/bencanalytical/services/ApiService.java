a8bb8fa5867a4ace6f616805c27b82f8d4dbaa10
fifo augie <fifoaugie06@gmail.com>
2020-08-04 22:31:56 +0700

    Tabbed Activity

--- app/src/main/java/com/example/bencanalytical/services/ApiService.java
+++ app/src/main/java/com/example/bencanalytical/services/ApiService.java
@@ -1,6 +1,16 @@
 package com.example.bencanalytical.services;
 
 
-public interface ApiService {
+import com.example.bencanalytical.model.ResponseGeneral;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @FormUrlEncoded
+    @POST("auth.php")
+    Call<ResponseGeneral> auth(@Field("username") String username,
+                               @Field("password") String password);
 }
