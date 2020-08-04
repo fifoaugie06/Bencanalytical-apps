4b6d414234f0e286ecb1edf59907d59f8c7e25ec
fifo augie <fifoaugie06@gmail.com>
2020-08-04 23:04:14 +0700

    Revert "Tabbed Activity"
    This reverts commit a8bb8fa5

--- app/src/main/java/com/example/bencanalytical/services/ApiClient.java
+++ app/src/main/java/com/example/bencanalytical/services/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "http://192.168.1.3/bencanalytical/";
+    public static final String BASE_URL = "http://192.168.1.6/telkom/";
     private static final String BASE_URL_API = BASE_URL + "api-mobile/";
     private static Retrofit retrofit = null;
 
--- app/src/main/java/com/example/bencanalytical/services/ApiService.java
+++ app/src/main/java/com/example/bencanalytical/services/ApiService.java
@@ -1,16 +1,6 @@
 package com.example.bencanalytical.services;
 
 
-import com.example.bencanalytical.model.ResponseGeneral;
-
-import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.POST;
-
 public interface ApiService {
-    @FormUrlEncoded
-    @POST("auth.php")
-    Call<ResponseGeneral> auth(@Field("username") String username,
-                               @Field("password") String password);
+
 }
